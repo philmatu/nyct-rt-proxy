1cb03a89a081082af0f4ec1230d2d74073330250
Simon Jacobs <sjacobs@camsys.com>
2017-03-28 11:33:48 -0400

    Remove stop matching code from LazyTripMatcher

--- src/main/java/com/kurtraschke/nyctrtproxy/services/LazyTripMatcher.java
+++ src/main/java/com/kurtraschke/nyctrtproxy/services/LazyTripMatcher.java
@@ -1,10 +1,8 @@
 package com.kurtraschke.nyctrtproxy.services;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.inject.Inject;
 import com.google.transit.realtime.GtfsRealtime;
-import com.google.transit.realtime.GtfsRealtime.TripUpdate.StopTimeUpdate;
 import com.kurtraschke.nyctrtproxy.model.ActivatedTrip;
 import com.kurtraschke.nyctrtproxy.model.NyctTripId;
 import com.kurtraschke.nyctrtproxy.model.TripMatchResult;
@@ -20,10 +18,8 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class LazyTripMatcher implements TripMatcher {
 
@@ -71,61 +67,6 @@ public class LazyTripMatcher implements TripMatcher {
   @Override
   public void initForFeed(Date start, Date end, Set<String> routeIds) {
     // do nothing
-  }
-
-  // No longer used - save the stop matching for after we check for merges.
-  // This is quite complicated because we initially wanted to see exactly where and what kind of extra stops or gaps there were.
-  private boolean tripMatch(Trip trip, List<StopTimeUpdate> stusWithTimepoints, List<StopTime> stopTimes) {
-    List<String> stops = stopTimes.stream().map(s -> s.getStop().getId().getId()).collect(Collectors.toList());
-    List<StopTimeUpdate> stus = stusWithTimepoints.stream()
-            .filter(stu -> isStopIdValid(stu.getStopId())).collect(Collectors.toList());
-
-    // Find longest sequence of stus that matches stops
-    List<List<StopTimeUpdate>> sequences = Lists.newArrayList();
-
-    int gtfsIdx = 0;
-
-    Iterator<StopTimeUpdate> stuIter = stus.iterator();
-    while (stuIter.hasNext()) {
-      StopTimeUpdate stu = stuIter.next();
-      while (gtfsIdx < stops.size() && !stops.get(gtfsIdx).equals(stu.getStopId()))
-        gtfsIdx++;
-
-      List<StopTimeUpdate> seq = Lists.newArrayList();
-
-      while (gtfsIdx < stops.size()) {
-        if (stu.getStopId().equals(stops.get(gtfsIdx))) {
-          seq.add(stu);
-          gtfsIdx++;
-        } else {
-          break;
-        }
-        if (stuIter.hasNext())
-          stu = stuIter.next();
-        else
-          break;
-      }
-      sequences.add(seq);
-    }
-
-    // there's a match if the last-added sequence ends with the last stop TODO longest? last added?
-
-    if (sequences.isEmpty())
-      return false;
-
-    if (sequences.size() > 1) {
-      Set<String> extras = Sets.newHashSet();
-      for (int i = 0; i < sequences.size() - 1; i++)
-        for (StopTimeUpdate stu : sequences.get(i))
-          extras.add(stu.getStopId());
-    }
-
-    // last stop ID is last STU: collections are exhausted at the same time
-    return !stuIter.hasNext() && gtfsIdx == stops.size();
-  }
-
-  private boolean isStopIdValid(String id) {
-    return _dao.getStopForId(new AgencyAndId("MTA NYCT", id)) != null;
   }
 
   // Find possible match candidates among static trips.
